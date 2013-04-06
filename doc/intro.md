57fb785e2880e20c22840644a2f5e62356154328
Eric Prunier <eric.prunier@gmail.com>
2013-04-06 10:55:40 +0200

    Initial import

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to incantations
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/incantations/core_test.clj
@@ -0,0 +1,7 @@
+(ns incantations.core-test
+  (:use clojure.test
+        incantations.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
